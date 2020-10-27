75002008d487431336f536e4a54c14344112f475
kristieniemiec <61203772+kristieniemiec@users.noreply.github.com>
2020-10-26 23:03:43 -0500

    Start of Overlay Menus
    -gaze.cs: handles gaze-based events
    -Blank overlay menu appears on item when user looks at it and disappears when user looks away
    -Added basic UI elements to display menu

---
+++ Assets/gaze.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gaze : MonoBehaviour
+{
+    public GameObject overlayMenu;
+    public Camera camera;
+    private bool isShowing = false;
+
+    public void Start()
+    {
+        overlayMenu.SetActive(isShowing);
+    }
+
+
+    public void OnGazeEnter()
+    {
+        // display menu
+        Debug.Log("Gaze entered");
+
+        overlayMenu.transform.position = camera.transform.position + camera.transform.forward * 3;
+        overlayMenu.transform.rotation = camera.transform.rotation;
+
+        isShowing = !isShowing;
+        overlayMenu.SetActive(isShowing);
+    }
+
+    public void OnGazeLeave()
+    {
+        // hide menu
+        Debug.Log("Gaze left");
+        isShowing = !isShowing;
+        overlayMenu.SetActive(isShowing);
+    }
+}
